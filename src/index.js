437be53ff4e6b8027d8ef7c7168c220087c1c8be
takashi1992 <takashi1992_sumi@outlook.jp>
2021-04-04 22:14:52 +0900

    material-ui index

--- src/index.js
+++ src/index.js
@@ -12,22 +12,25 @@ import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
 import EventsShow from './components/events_show';
 import reportWebVitals from './reportWebVitals';
+import MuthemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 const enhancer = process.env.NODE_ENV === 'development' ?
   composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
 const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/events/new" component={EventsNew}/>
-        <Route path="/events/:id" component={EventsShow}/>
-        <Route exact path="/" component={EventsIndex}/>
-        <Route exact path="/events" component={EventsIndex}/>
-      </Switch>
-    </BrowserRouter>
-  </Provider>,
+  <MuthemeProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/events/new" component={EventsNew}/>
+          <Route path="/events/:id" component={EventsShow}/>
+          <Route exact path="/" component={EventsIndex}/>
+          <Route exact path="/events" component={EventsIndex}/>
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  </MuthemeProvider>,
   document.getElementById('root')
 );
 
