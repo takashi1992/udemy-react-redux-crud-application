36532205a4ce8171b135f6a6c7c4c24de9b7cec3
takashi1992 <takashi1992_sumi@outlook.jp>
2021-04-04 21:43:52 +0900

    put get delete Event

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -29,14 +29,13 @@ class EventsNew extends Component {
   }
 
   render(){
-    const {handleSubmit, pristine, submitting} = this.props
-    console.log(submitting)
+    const {handleSubmit, pristine, submitting, invalid} = this.props
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div><Field label="Title" name="title" type="text" component={ this.renderField }/></div>
         <div><Field label="Body" name="body" type="text" component={ this.renderField }/></div>
         <div>
-            <input type="submit" value="submit" disabled={pristine || submitting}/>
+            <input type="submit" value="submit" disabled={pristine || submitting || invalid}/>
             <Link to="/">Cancel</Link>
         </div>
       </form>
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,8 +1,13 @@
 import _ from 'lodash'
-import { READ_EVENTS,DELETE_EVENT } from "../actions";
+import { READ_EVENTS,DELETE_EVENT,READ_EVENT,UPDATE_EVENT,CREATE_EVENT } from "../actions";
 
 export default (events = {}, action) => {
   switch (action.type) {
+    case UPDATE_EVENT:
+    case CREATE_EVENT:
+    case READ_EVENT:
+      const data = action.response.data
+      return { ...events, [data.id]: data}
     case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id')
     case DELETE_EVENT:
