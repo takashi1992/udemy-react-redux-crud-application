e832dbc1f9cb6e45373722fd498cc2d95a66661a
takashi1992 <takashi1992_sumi@outlook.jp>
2021-04-04 22:42:38 +0900

    material-ui

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Field, reduxForm } from 'redux-form'
+import Button from '@material-ui/core/Button'
+import TextField from 'material-ui/TextField'
 
 import { getEvent, deleteEvent, putEvent } from '../actions'
 
@@ -22,10 +24,14 @@ class EventsShow extends Component {
       const {input , label, type, meta: {touched,error}} = field
 
       return (
-      <div>
-          <input {...input} placeholder={label} type={type}/>
-          {touched && error && <span>{error}</span>}
-      </div>
+      <TextField
+        hintText={label}
+        floatingLabelText={label}
+        type={type}
+        errorText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
       )
   }
 
@@ -42,15 +48,14 @@ class EventsShow extends Component {
 
   render(){
     const {handleSubmit, pristine, submitting, invalid} = this.props
+    const style = {margin: 12, wirth: '100%'}
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div><Field label="Title" name="title" type="text" component={ this.renderField }/></div>
         <div><Field label="Body" name="body" type="text" component={ this.renderField }/></div>
-        <div>
-            <input type="submit" value="put" disabled={pristine || submitting || invalid}/>
-            <Link to="/">Cancel</Link>
-            <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
-        </div>
+        <Button variant="contained" type="submit" style={style} disabled={pristine || submitting || invalid}>Submit</Button>
+        <Link to="/"><Button variant="contained" style={style}>Cancel</Button></Link>
+        <Button variant="contained" style={style} onClick={this.onDeleteClick}>Delete</Button>
       </form>
     )
   }
